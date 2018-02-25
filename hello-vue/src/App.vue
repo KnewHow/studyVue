4cdff2385d2325323f2c8ab0174a54d72f899eb0
KnewHow <948170910@qq.com>
2018-02-25 20:34:49 +0800

    hello-play

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app-2">
+    lala
+    {{ msg }}
+    <firstcomponent></firstcomponent>
+    <secondcomponet></secondcomponet>
+  </div>
+
+</template>
+
+<script>
+import firstcomponent from './component/firstcomponent.vue'
+export default{
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  components: {firstcomponent}
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ hello-vue/src/cdd.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="cdd">lala</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scs">
+
+</style>
